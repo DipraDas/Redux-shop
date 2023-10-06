e1fe38cb3c7e83b2cd1fdf044612c96b80f761f7
Dipra <dipradas5940@gmail.com>
2023-10-07 00:16:45 +0600

    Making First get Req

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:6000' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:6060' }),
     endpoints: (builder) => ({
         getProducts: builder.query({
             query: () => '/products'
