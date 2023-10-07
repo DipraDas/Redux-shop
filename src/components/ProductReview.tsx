9ce22ee2014257aa4b620540ec88108982f90c92
Dipra <dipradas5940@gmail.com>
2023-10-07 13:04:33 +0600

    Cache-revalidation

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -3,14 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 import { FiSend } from 'react-icons/fi';
-import { usePostCommentMutation } from '@/redux/api/apiSlice';
-
-const dummyComments = [
-  'Bhalo na',
-  'Ki shob ghori egula??',
-  'Eta kono product holo ??',
-  '200 taka dibo, hobe ??',
-];
+import { useGetCommentQuery, usePostCommentMutation } from '@/redux/api/apiSlice';
 
 interface IProps {
   id: string;
@@ -22,6 +15,8 @@ export default function ProductReview({ id }: IProps) {
   console.log(isLoading)
   console.log(isError)
   console.log(isSuccess)
+
+  const {data} = useGetCommentQuery(id);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     console.log(inputValue);
@@ -54,7 +49,7 @@ export default function ProductReview({ id }: IProps) {
         </Button>
       </form>
       <div className="mt-10">
-        {dummyComments.map((comment, index) => (
+        {data?.comments?.map((comment: string, index: number) => (
           <div key={index} className="flex gap-3 items-center mb-5">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:6060' }),
+    tagTypes: ['comments'], // ! Used for real time update
     endpoints: (builder) => ({
         getProducts: builder.query({
             query: () => '/products'
@@ -15,13 +16,19 @@ export const api = createApi({
                 url: `/comment/${id}`,
                 method: 'POST',
                 body: data
-            })
-        })
+            }),
+            invalidatesTags: ['comments'] // ! Used for clear the cache for updating new data here
+        }),
+        getComment: builder.query({
+            query: (id) => `/comment/${id}`,
+            providesTags: ['comments'] // ! Used for real time update as result
+        }),
     })
 })
 
 export const {
     useGetProductsQuery,
     useSingleProductQuery,
-    usePostCommentMutation
+    usePostCommentMutation,
+    useGetCommentQuery
 } = api;
